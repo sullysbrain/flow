cb81545c8d4a5af55656baa74b2d4214b4eb9fc6
Scott Sullivan <scott@sullysbrain.com>
2022-05-13 17:54:20 -0400

    Django server functional and now runs in base config.

--- flow_project/wsgi.py
+++ flow_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for flow project.
+WSGI config for flow_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow_project.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flow_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
