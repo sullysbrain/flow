99b5bca75d3cfd091126aa462bcfb30a46e2b070
Scott Sullivan <scott@sullysbrain.com>
2022-05-14 17:41:15 -0400

    Added base views and roughed in templates for the List View.

--- flow_app/urls.py
+++ flow_app/urls.py
@@ -1,4 +1,10 @@
+# todo_list/flow_app/urls.py
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/",
+        views.ItemListView.as_view(), name="list"),
 ]
