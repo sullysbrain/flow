171781ae37d5cc39f38c51709e8799af70368069
Scott Sullivan <scott@sullysbrain.com>
2022-05-16 17:43:43 -0400

    Began testing building, instantiating, and calling (in Python shell) objects from the database.

--- flow_app/models.py
+++ flow_app/models.py
@@ -5,10 +5,18 @@ from django.urls import reverse
 def one_week_hence():
     return timezone.now() + timezone.timedelta(days=7)
 
+class Project(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
 class Employee(models.Model):
-    pass
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
 
-
+    def __str__(self):
+        return self.first_name + self.last_name
 
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
