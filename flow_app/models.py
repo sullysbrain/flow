4baede9b39512e4342765d385061ac5a77b2c9cd
Scott Sullivan <scott@sullysbrain.com>
2022-05-18 20:20:39 -0400

    Continued database model tweaks.

--- flow_app/models.py
+++ flow_app/models.py
@@ -16,7 +16,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + " " + self.last_name
 
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
